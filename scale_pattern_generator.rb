49b1590941ce4fef54c8a4306a1fe20fb384d5b8
Grant Davis <grantdavis303@gmail.com>
2025-01-11 12:07:46 -0700

    Update Scale Pattern Generator

--- scale_pattern_generator.rb
+++ scale_pattern_generator.rb
@@ -1,78 +1,94 @@
 require 'pry'
+require './text_block.rb'
 
-puts "Welcome to the Scale Pattern Generator! \n"
-puts "\n"
-puts "1. C Major = C, D, E, F, G, A, B, C"
-puts "2. D Major = D, E, F♯, G, A, B, C♯, D"
-puts "3. E Major = E, F♯, G♯, A, B, C♯, D♯, E"
-puts "4. F Sharp Major = F♯, G♯, A♯, B, C♯, D♯, E♯, F♯"
-puts "5. F Major = F, G, A, B♭, C, D, E, F"
-puts "6. E Flat Major = E♭, F, G, A♭, B♭, C, D, E♭"
-puts "7. D Flat Major = D♭, E♭, F, G♭, A♭, B♭, C, D♭"
-puts "8. G Major = G, A, B, C, D, E, F♯, G"
-puts "9. A Major = A, B, C♯, D, E, F♯, F♯, A"
-puts "10. B Major = B, C♯, D♯, E, F♯, G♯, A♯, B"
-puts "11. C Sharp Major = C♯, D♯, E♯, F♯, G♯, A♯, B♯, C♯"
-puts "12. B Flat Major = B♭, C, D, E♭, F, G, A, B♭"
-puts "13. A Flat Major = A♭, B♭, C, D♭, E♭, F, G, A♭"
-puts "14. G Flat Major = G♭, A♭, B♭, C♭, D♭, E♭, F, G♭"
-puts "\n"
+intro_text = File.read('./text_blocks/intro_text.txt')
+major_scales_list = File.read('./text_blocks/major_scales_list.txt')
+minor_scales_list = File.read('./text_blocks/minor_scales_list.txt')
 
-puts "Which Scale? Please choose a number between 1 and 14."
-user_input_scale = gets.chomp.to_i
+intro_text_block = TextBlock.new("Intro Block Text", intro_text)
+major_scale_text_block = TextBlock.new("Major Scale List", major_scales_list)
+minor_scales_text_block = TextBlock.new("Minor Scale List", minor_scales_list)
 
-if user_input_scale == 1
-  scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C']
-elsif user_input_scale == 2
-  scale = ['D', 'E', 'F♯', 'G', 'A', 'B', 'C♯', 'D']
-elsif user_input_scale == 3
-  scale = ['E', 'F♯', 'G♯', 'A', 'B', 'C♯', 'D♯', 'E']
-elsif user_input_scale == 4
-  scale = ['F♯', 'G♯', 'A♯', 'B', 'C♯', 'D♯', 'E♯', 'F♯']
-elsif user_input_scale == 5
-  scale = ['F', 'G', 'A', 'B♭', 'C', 'D', 'E', 'F']
-elsif user_input_scale == 6
-  scale = ['E♭', 'F', 'G', 'A♭', 'B♭', 'C', 'D', 'E♭']
-elsif user_input_scale == 7
-  scale = ['D♭', 'E♭', 'F', 'G♭', 'A♭', 'B♭', 'C', 'D♭']
-elsif user_input_scale == 8
-  scale = ['G', 'A', 'B', 'C', 'D', 'E', 'F♯', 'G']
-elsif user_input_scale == 9
-  scale = ['A', 'B', 'C♯', 'D', 'E', 'F♯', 'F♯', 'A']
-elsif user_input_scale == 10
-  scale = ['B', 'C♯', 'D♯', 'E', 'F♯', 'G♯', 'A♯', 'B']
-elsif user_input_scale == 11
-  scale = ['C♯', 'D♯', 'E♯', 'F♯', 'G♯', 'A♯', 'B♯', 'C♯']
-elsif user_input_scale == 12
-  scale = ['B♭', 'C', 'D', 'E♭', 'F', 'G', 'A', 'B♭']
-elsif user_input_scale == 13
-  scale = ['A♭', 'B♭', 'C', 'D♭', 'E♭', 'F', 'G', 'A♭']
-elsif user_input_scale == 14
-  scale = ['G♭', 'A♭', 'B♭', 'C♭', 'D♭', 'E♭', 'F', 'G♭']
+# Scale Pattern Generator Program
+
+puts intro_text_block.content
+
+scales_list_input = gets.chomp.to_i
+
+if scales_list_input == 1
+  puts major_scale_text_block.content
+elsif scales_list_input == 2
+  puts minor_scales_text_block.content
 else
   puts "Invalid input. Please try again."
   return
 end
 
-puts "\n"
+puts "Which Scale? Please choose a number between 1 and 30."
+user_input_scale = gets.chomp.to_i
 
-puts "Beat Count? Please choose between 2, 4, 8, 16, and 32."
-beat_count = gets.chomp.to_i
 
-puts "\n"
 
-if [2, 4, 8, 16, 32].include?(beat_count)
-  new_scale_pattern = Array.new
 
-  length = scale.length
 
-  beat_count.times do
-    index_position = rand(0...length)
-    new_scale_pattern << scale[index_position]
-  end
 
-  puts "Generated pattern:"
-  p new_scale_pattern
-else
-  puts "Invalid input. Please try again."
-end
+
+
+
+# # These are wrong - change
+
+# if user_input_scale == 1
+#   scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C']
+# elsif user_input_scale == 2
+#   scale = ['D', 'E', 'F♯', 'G', 'A', 'B', 'C♯', 'D']
+# elsif user_input_scale == 3
+#   scale = ['E', 'F♯', 'G♯', 'A', 'B', 'C♯', 'D♯', 'E']
+# elsif user_input_scale == 4
+#   scale = ['F♯', 'G♯', 'A♯', 'B', 'C♯', 'D♯', 'E♯', 'F♯']
+# elsif user_input_scale == 5
+#   scale = ['F', 'G', 'A', 'B♭', 'C', 'D', 'E', 'F']
+# elsif user_input_scale == 6
+#   scale = ['E♭', 'F', 'G', 'A♭', 'B♭', 'C', 'D', 'E♭']
+# elsif user_input_scale == 7
+#   scale = ['D♭', 'E♭', 'F', 'G♭', 'A♭', 'B♭', 'C', 'D♭']
+# elsif user_input_scale == 8
+#   scale = ['G', 'A', 'B', 'C', 'D', 'E', 'F♯', 'G']
+# elsif user_input_scale == 9
+#   scale = ['A', 'B', 'C♯', 'D', 'E', 'F♯', 'F♯', 'A']
+# elsif user_input_scale == 10
+#   scale = ['B', 'C♯', 'D♯', 'E', 'F♯', 'G♯', 'A♯', 'B']
+# elsif user_input_scale == 11
+#   scale = ['C♯', 'D♯', 'E♯', 'F♯', 'G♯', 'A♯', 'B♯', 'C♯']
+# elsif user_input_scale == 12
+#   scale = ['B♭', 'C', 'D', 'E♭', 'F', 'G', 'A', 'B♭']
+# elsif user_input_scale == 13
+#   scale = ['A♭', 'B♭', 'C', 'D♭', 'E♭', 'F', 'G', 'A♭']
+# elsif user_input_scale == 14
+#   scale = ['G♭', 'A♭', 'B♭', 'C♭', 'D♭', 'E♭', 'F', 'G♭']
+# else
+#   puts "Invalid input. Please try again."
+#   return
+# end
+
+# puts "\n"
+
+# puts "Beat Count? Please choose between 2, 4, 8, 16, and 32."
+# beat_count = gets.chomp.to_i
+
+# puts "\n"
+
+# if [2, 4, 8, 16, 32].include?(beat_count)
+#   new_scale_pattern = Array.new
+
+#   length = scale.length
+
+#   beat_count.times do
+#     index_position = rand(0...length)
+#     new_scale_pattern << scale[index_position]
+#   end
+
+#   puts "Generated pattern:"
+#   p new_scale_pattern
+# else
+#   puts "Invalid input. Please try again."
+# end
